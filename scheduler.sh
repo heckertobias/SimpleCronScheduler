9ae4df20e76647e24dacad38783f0eaf08b3ed47
Tobias Hecker <heckertobias@googlemail.com>
2017-08-11 16:37:56 +0200

    first version of control script

--- scheduler.sh
+++ scheduler.sh
@@ -5,26 +5,43 @@
 
 source ./config.sh
 
+function help {
+  
+  echo "SimpleCronScheduler usage:"
+  echo " scheduler add ?    - add a script placed in $SCRIPTDIR to the queue"
+  echo " scheduler remove ? - removes all occurrence of ? from the queue"
+  echo " scheduler reset    - clears the complete queue"
+}
+
 if [ $# -eq 1 ]
   then
-    if [ $1 = "clear" ]
+    if [ $1 = "reset" ]
       then
-        echo "CLEAR"
-    elif [ $1 = "reset" ]
-      then
-        echo "RESET"
+        echo "" > $JOBFILE
+        kill (echo $LOCKFILE)
+        rm -f $LOCKFILE
+        
+        echo "SimpleCronScheduler was successfully resetted"
     else
-      echo "help"
+      help
     fi
 elif [ $# -eq 2 ]
   then
     if [ $1 = "add" ]
       then
-        echo "ADD $2"
+        if [ -f $SCRIPTDIR$2 ]
+          then
+            chmod +x $SCRIPTDIR$2
+            echo $2 >> $JOBFILE
+            
+            echo "$2 was added to SimpleCronSchedulers queue"
+        else
+          echo "Script $2 does not exit in $SCRIPTDIR"
+        fi
     elif [ $1 = "remove" ]
       then
-        echo "REMOVE $2"
+        echo "REMOVE is currently not implemented in SimpleCronScheduler"
     else
-      echo "help"
+      help
     fi
 fi
